995c03cae2991457d9e98147236d542b0fbf6470
dolphin <fun53@ukr.net>
2023-04-26 14:54:30 +0300

    a1

--- src/js/sale.js
+++ src/js/sale.js
@@ -1,6 +1,6 @@
 
 let indexHorizontal = 1;
-showHorizontal(indexHorizontal);
+// showHorizontal(indexHorizontal);
 
 // Next/previous controls
 
@@ -9,23 +9,23 @@ showHorizontal(indexHorizontal);
 // }
 
 // Thumbnail image controls
-window.currentSlide = function (n) {
-    showHorizontal(indexHorizontal = n);
-}
+// window.currentSlide = function (n) {
+//     showHorizontal(indexHorizontal = n);
+// }
 
 
-window.showHorizontal = function (n) {
-    let i;
-    let slides = document.getElementsByClassName("slideHorizontal");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {indexHorizontal = 1}
-    if (n < 1) {indexHorizontal = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[indexHorizontal-1].style.display = "block";
-    dots[indexHorizontal-1].className += " active";
-}
+// window.showHorizontal = function (n) {
+//     let i;
+//     let slides = document.getElementsByClassName("slideHorizontal");
+//     let dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {indexHorizontal = 1}
+//     if (n < 1) {indexHorizontal = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[indexHorizontal-1].style.display = "block";
+//     dots[indexHorizontal-1].className += " active";
+// }
