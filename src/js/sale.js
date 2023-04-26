703b434aea485f64db8edbf5532f1bc49dc55fe4
dolphin <fun53@ukr.net>
2023-04-26 14:44:27 +0300

    Update sale.js

--- src/js/sale.js
+++ src/js/sale.js
@@ -4,17 +4,17 @@ showHorizontal(indexHorizontal);
 
 // Next/previous controls
 
-function plusHorizontal(n) {
-    showHorizontal(indexHorizontal += n);
+// function plusHorizontal(n) {
+//     showHorizontal(indexHorizontal += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+window.currentSlide = function (n) {
     showHorizontal(indexHorizontal = n);
 }
 
 
-function showHorizontal(n) {
+window.showHorizontal = function (n) {
     let i;
     let slides = document.getElementsByClassName("slideHorizontal");
     let dots = document.getElementsByClassName("dot");
@@ -23,9 +23,9 @@ function showHorizontal(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[indexHorizontal-1].style.display = "block";
-    // dots[indexHorizontal-1].className += " active";
+    dots[indexHorizontal-1].className += " active";
 }
